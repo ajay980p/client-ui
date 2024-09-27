ffa7ba7f4703078fcc7775a3671040bb3af472a1
ajay980p <ajay980p@gmail.com>
2024-09-27 23:21:54 +0530

    commit changes

--- src/components/custom/choosePizzaTopping.tsx
+++ src/components/custom/choosePizzaTopping.tsx
@@ -3,11 +3,12 @@
 import Image from 'next/image'
 import { useState } from 'react'
 import { Label } from "@/components/ui/label"
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+import { CardContent } from "@/components/ui/card"
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
 import HomePageImg from "@/public/assets/pizza_homepage_img.png";
 import PizzaCard from "./pizzaCard";
-import { Dialog, DialogContent } from '@radix-ui/react-dialog'
+import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog";
+
 
 export default function ChoosePizzaTopping({ isModalOpen, setIsModalOpen }: { isModalOpen: boolean, setIsModalOpen: React.Dispatch<React.SetStateAction<boolean>> }) {
     const [size, setSize] = useState<string>('medium')
@@ -15,56 +16,54 @@ export default function ChoosePizzaTopping({ isModalOpen, setIsModalOpen }: { is
 
     return (
         <Dialog open={isModalOpen} onOpenChange={setIsModalOpen}>
-            <DialogContent className="sm:max-w-[425px]">
+            <DialogContent className="sm:max-w-[425px] sm:max-h-[600px]">
 
-                <Card className="w-[350px] bg-gray-50">
-                    <CardHeader className='p-0 pt-4'>
-                        <CardTitle className='text-center'>Margarita pizza</CardTitle>
-                        <p className="text-sm text-muted-foreground text-center">This is a very healthy pizza</p>
-                    </CardHeader>
+                <DialogHeader>
+                    <DialogTitle className="text-center text-2xl font-bold text-orange-500">Welcome</DialogTitle>
+                    <p className="text-sm text-muted-foreground text-center">This is a very healthy pizza</p>
+                </DialogHeader>
 
-                    <CardContent className="space-y-1">
-                        <div className="flex justify-center">
-                            <Image src={HomePageImg} alt="Pizza" width={150} height={150} />
-                        </div>
+                <CardContent >
+                    <div className="flex justify-center">
+                        <Image src={HomePageImg} alt="Pizza" width={150} height={150} />
+                    </div>
 
-                        <div>
-                            <h3 className="mb-2 font-semibold">Choose the size</h3>
-                            <RadioGroup defaultValue={size} onValueChange={setSize} className="flex space-x-2">
-                                <div className="flex items-center space-x-2">
-                                    <RadioGroupItem value="small" id="small" />
-                                    <Label htmlFor="small">Small</Label>
-                                </div>
-                                <div className="flex items-center space-x-2">
-                                    <RadioGroupItem value="medium" id="medium" />
-                                    <Label htmlFor="medium">Medium</Label>
-                                </div>
-                                <div className="flex items-center space-x-2">
-                                    <RadioGroupItem value="large" id="large" />
-                                    <Label htmlFor="large">Large</Label>
-                                </div>
-                            </RadioGroup>
-                        </div>
+                    <div>
+                        <h3 className="mb-2 font-semibold">Choose the size</h3>
+                        <RadioGroup defaultValue={size} onValueChange={setSize} className="flex space-x-2">
+                            <div className="flex items-center space-x-2">
+                                <RadioGroupItem value="small" id="small" />
+                                <Label htmlFor="small">Small</Label>
+                            </div>
+                            <div className="flex items-center space-x-2">
+                                <RadioGroupItem value="medium" id="medium" />
+                                <Label htmlFor="medium">Medium</Label>
+                            </div>
+                            <div className="flex items-center space-x-2">
+                                <RadioGroupItem value="large" id="large" />
+                                <Label htmlFor="large">Large</Label>
+                            </div>
+                        </RadioGroup>
+                    </div>
 
-                        <div className='py-4'>
-                            <h3 className="mb-2 font-semibold">Choose the crust</h3>
-                            <RadioGroup defaultValue={crust} onValueChange={setCrust} className="flex space-x-2">
-                                <div className="flex items-center space-x-2">
-                                    <RadioGroupItem value="thin" id="thin" />
-                                    <Label htmlFor="thin">Thin</Label>
-                                </div>
-                                <div className="flex items-center space-x-2">
-                                    <RadioGroupItem value="thick" id="thick" />
-                                    <Label htmlFor="thick">Thick</Label>
-                                </div>
-                            </RadioGroup>
-                        </div>
+                    <div className='py-4'>
+                        <h3 className="mb-2 font-semibold">Choose the crust</h3>
+                        <RadioGroup defaultValue={crust} onValueChange={setCrust} className="flex space-x-2">
+                            <div className="flex items-center space-x-2">
+                                <RadioGroupItem value="thin" id="thin" />
+                                <Label htmlFor="thin">Thin</Label>
+                            </div>
+                            <div className="flex items-center space-x-2">
+                                <RadioGroupItem value="thick" id="thick" />
+                                <Label htmlFor="thick">Thick</Label>
+                            </div>
+                        </RadioGroup>
+                    </div>
 
-                        <div>
-                            <PizzaCard />
-                        </div>
-                    </CardContent>
-                </Card>
+                    <div>
+                        <PizzaCard />
+                    </div>
+                </CardContent>
 
             </DialogContent>
 
